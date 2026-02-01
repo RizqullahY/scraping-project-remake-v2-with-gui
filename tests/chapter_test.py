a7291b718ab2c1b6801c01986cb8dab8a7d2f200
Rafly R. Y. <rizqullahyusufrafly@gmail.com>
2026-02-01 20:47:03 +0700

    CHORE: prepare for unittest

---
+++ tests/chapter_test.py
@@ -0,0 +1,15 @@
+from modules import komikindo_chapter
+from modules import komiku_chapter
+from modules import softkomik_chapter
+from modules import shinigami_chapter
+
+komiku_chapter_scrape_url = 'https://asdasd'
+shinigami_chapter_scrape_url = 'https://asdasd'
+komikindo_chapter_scrape_url = 'https://asdasd'
+softkomik_chapter_scrape_url = 'https://asdasd'
+
+komikindo_chapter.scrape_judulseries(komikindo_chapter_scrape_url)
+komiku_chapter.scrape_judulseries(komiku_chapter_scrape_url)
+softkomik_chapter.scrape_judulseries(softkomik_chapter_scrape_url)
+shinigami_chapter.shinigami_scrape_series(shinigami_chapter_scrape_url)
+
---
+++ tests/image_test.py
@@ -0,0 +1,12 @@
+from modules import komikindo_image
+from modules import komiku_image
+from modules import softkomik_image
+from modules import shinigami_image
+
+example_image_komiku_url = 'https://asdasd'
+example_image_shinigami_url = 'https://asdasd'
+example_image_komikindo_url = 'https://asdasd'
+example_image_softkomik_url = 'https://asdasd'
+
+komikindo_image.download_image(example_image_komiku_url)
+
